47ccc91380b6dd6b9d7843d49fbfb4f2ef5f51b7
vendymasarova <vendymasarova@seznam.cz>
2021-12-06 17:08:27 +0100

    My first svelte application

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+import FeedbackForm from "./components/FeedbackForm.svelte";
+
+
+	import FeedbackList from "./components/FeedbackList.svelte"
+import FeedbackStats from "./components/FeedbackStats.svelte";
+	
+	import "./global.css"
+
+	
+</script>
+
+<main class="container">
+	<FeedbackForm />
+	<FeedbackStats/>
+	<FeedbackList/>
+</main>
+
